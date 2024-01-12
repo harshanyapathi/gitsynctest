e116fd6cd4d6896ef4477a31aeb7f7cb15b8808b
harshanyapathi <harsha.nyapathi@bdb.ai>
2024-01-12 13:51:40 +0530

    changing credentials

--- mongo_conn.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-def create_mongo_connection():
-    uri='mongodb+srv://root:root@cluster0.acekv.mongodb.net/test?authSource=admin&replicaSet=atlas-svib4e-shard-0&readPreference=primary&ssl=true'
-    client=MongoClient(uri)
-    return client
